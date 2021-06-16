5d2622e517f12de049b7de1c6a5b314f7528d84d
Sam Baker <rb.sam@icloud.com>
2021-06-16 18:37:06 +0100

    added update and delete item functionality to bag

--- codecompanion/bag/urls.py
+++ codecompanion/bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name="view_bag"),
     path('add/<service_name>/', views.add_to_bag, name="add_to_bag"),
+    path('adjust/<service_name>/', views.adjust_bag, name="adjust_bag"),
+    path('remove/<service_name>/', views.remove_from_bag, name="remove_from_bag"),
 ]
