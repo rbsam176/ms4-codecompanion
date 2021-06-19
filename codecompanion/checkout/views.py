d5605415496f47a28ca7a0592412d2046d736a4c
Sam Baker <rb.sam@icloud.com>
2021-06-19 15:45:44 +0100

    added beginning checkout functionality

--- codecompanion/checkout/views.py
+++ codecompanion/checkout/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import redirect, render, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.context_processors import bag_contents
+
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at this time")
         return redirect(reverse('services'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': "pk_test_51J3lGyJNKRSX8nglocosYsPhCS7URkjyaTdlbIDI4E4XHXuhpUDWE6dDB5Yjdw9NhWpAM2ScQ7MFk5o5f0wYXyf700wZee66K7",
-        'client_secret': "test client secret",
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- codecompanion/codecompanion/settings.py
+++ codecompanion/codecompanion/settings.py
@@ -172,3 +172,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
