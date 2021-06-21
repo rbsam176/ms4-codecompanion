c15c9302fac3e226c7c32d35eab3ea1877d6bdb6
Sam Baker <rb.sam@icloud.com>
2021-06-21 19:29:20 +0100

    fixed typo in aws settings

--- codecompanion/settings.py
+++ codecompanion/settings.py
@@ -189,7 +189,7 @@ if 'USER_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFIELDS_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
