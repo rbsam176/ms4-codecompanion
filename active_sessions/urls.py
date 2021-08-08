4181b4864f15c603da4d9a202f1dbc9be34d0d20
Sam Baker <rb.sam@icloud.com>
2021-08-08 15:53:08 +0100

    added jitsi api to active session view

--- active_sessions/urls.py
+++ active_sessions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.active_sessions, name="active_sessions"),
+    path('<order_number>/', views.active_sessions, name='active_sessions'),
+    path('', views.redirect_services),
 ]
--- active_sessions/views.py
+++ active_sessions/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
 from django.contrib.auth.models import User
+
 from profiles.forms import UserProfile
+from checkout.models import Order, OrderLineItem
+
+import datetime
+import pytz
+
+def redirect_services(request):
+	return redirect('/profile')
 
 @login_required
-def active_sessions(request):
+def active_sessions(request, order_number):
+    print(order_number)
     account = get_object_or_404(User, username=request.user)
-    profile = get_object_or_404(UserProfile, user=request.user)
-    # orders = profile.orders.filter(active=True).order_by('-date')
+    order = get_object_or_404(Order, order_number=order_number)
 
-    context = {
-        'account': account,
-        'profile': profile,
-        # 'orders': orders,
-    }
+    # CHECK FOR ACTIVE SESSIONS
+    lineitems = OrderLineItem.objects.filter(order=order)
+    session = None
+    timezone = pytz.timezone("UTC")
+    current_ts = timezone.localize(datetime.datetime.now())
+    for item in lineitems:
+        seconds_until = (item.start_datetime - current_ts).total_seconds()
+        # IF WITHIN 5 MIN OF SESSION START TIME, OR CURRENT TIME IS START TIME, OR CURRENT TIME IS BETWEEN START AND END TIME
+        if seconds_until < 300 or current_ts == item.start_datetime or current_ts > item.start_datetime and current_ts < item.end_datetime:
+            session = item
 
-    return render(request, 'active_sessions/active_sessions.html', context)
+    if session:
+        context = {
+            'account': account,
+            'order': order,
+            'session': session,
+        }
+        return render(request, 'active_sessions/active_sessions.html', context)
+    else:
+        return redirect('/profile')
